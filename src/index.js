37dfbfb6fe66e6876e3c5fb737ee5ba3b9a7e558
osroot <osamaessayed.el.4468@azhar.edu.eg>
2023-01-24 12:02:43 -0500

    Set up React App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import react from 'react';
+// import react from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+const page = (
+    <div>
+        <h1>My Awesome website in React</h1>
+        <h3>Reasons I Love React</h3>
+        <ol>
+            <li>it's composable</li>
+            <li>it's declarative</li>
+            <li>it's hairable skill</li>
+            <li>it's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(
+    page,
+    document.getElementById('root')
+)
